a1ed48e57ff6dd506b12cd8eb1f9a572322bc4f4
mizne <w20054319@126.com>
2017-07-19 18:31:29 +0800

    update

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -23,6 +23,21 @@ const actions = {
     })
   },
   FETCH_DELIVERY_FEE: ({ commit }) => {
+    var geolocation = new BMap.Geolocation()
+    geolocation.getCurrentPosition(
+      function(r) {
+        if (this.getStatus() == BMAP_STATUS_SUCCESS) {
+          var mk = new BMap.Marker(r.point)
+          map.addOverlay(mk)
+          map.panTo(r.point)
+          alert('您的位置：' + r.point.lng + ',' + r.point.lat)
+        } else {
+          alert('failed' + this.getStatus())
+        }
+      },
+      { enableHighAccuracy: true }
+    )
+
     if ('geolocation' in navigator) {
       navigator.geolocation.getCurrentPosition(function(position) {
         console.log(position.coords.latitude, position.coords.longitude)
