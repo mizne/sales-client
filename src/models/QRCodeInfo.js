2e1844d713ead501f7d3fce77fe927342f006c46
mizne <w20054319@126.com>
2017-07-21 18:00:51 +0800

    update

--- src/models/QRCodeInfo.js
+++ src/models/QRCodeInfo.js
@@ -1,6 +1,16 @@
 import storage from '@/util/storage'
 import { DEAL, ESHOP } from '@/util/constants'
 
+const methods = [
+  'bizType',
+  'tenantId',
+  'tableName',
+  'consigneeId',
+  'coupons',
+  'couponRate',
+  'tableUser',
+  'phoneNumber'
+]
 const QRCodeInfo = function() {}
 
 QRCodeInfo.prototype.isDealBizType = function() {
@@ -12,71 +22,31 @@ QRCodeInfo.prototype.isEShopBizType = function() {
 }
 
 // 批量添加 set 方法
-;[
-  'bizType',
-  'tenantId',
-  'tableName',
-  'consigneeId',
-  'coupons',
-  'couponRate',
-  'tableUser',
-  'phoneNumber'
-].forEach(method => {
+methods.forEach(method => {
+  // 添加 set 方法
   QRCodeInfo.prototype['set' + capital(method)] = function(v) {
     storage.set(method, v)
     return this
   }
-})
 
-// 批量添加 get 方法
-;[
-  'bizType',
-  'tenantId',
-  'tableName',
-  'consigneeId',
-  'coupons',
-  'couponRate',
-  'tableUser',
-  'phoneNumber'
-].forEach(method => {
+  // 添加 get 方法
   QRCodeInfo.prototype['get' + capital(method)] = function() {
     return storage.get(method)
   }
-})
 
-// 批量添加 has 方法
-;[
-  'bizType',
-  'tenantId',
-  'tableName',
-  'consigneeId',
-  'coupons',
-  'couponRate',
-  'tableUser',
-  'phoneNumber'
-].forEach(method => {
+  // 添加 has 方法
   QRCodeInfo.prototype['has' + capital(method)] = function(v) {
     return this['get' + capital(method)]() != null
   }
-})
 
-// 批量添加 remove 方法
-;[
-  'bizType',
-  'tenantId',
-  'tableName',
-  'consigneeId',
-  'coupons',
-  'couponRate',
-  'tableUser',
-  'phoneNumber'
-].forEach(method => {
+  // 添加 remove 方法
   QRCodeInfo.prototype['remove' + capital(method)] = function() {
     storage.remove(method)
     return this
   }
 })
 
+// 将字符串 s 首字母大写
 export function capital(s) {
   return s[0].toUpperCase() + s.slice(1)
 }
