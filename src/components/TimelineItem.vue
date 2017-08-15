652471bf6e3567fb0bc102b079f7dfaf0826247b
mizne <w20054319@126.com>
2017-08-15 17:33:55 +0800

    update

--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -1,7 +1,7 @@
 <template>
 	<li class="vux-timeline-item">
 		<div :class="['vux-timeline-item-color', {'vux-timeline-item-head': !current,'vux-timeline-item-head-first': current }]" :style="headStyle">
-			<icon v-show="current" type="success_no_circle" class="vux-timeline-item-checked"></icon>
+			<!-- <icon v-show="current" type="success_no_circle" class="vux-timeline-item-checked"></icon> -->
 		</div>
 		<div class="vux-timeline-item-tail" :style="tailStyle"></div>
 		<div class="vux-timeline-item-content" :class="{'past': past, 'current': current, 'future': future}">
