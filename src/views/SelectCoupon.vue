4db37ac3c55e11418a41f342bd5048612b982120
mizne <w20054319@126.com>
2017-08-17 20:05:34 +0800

    update

--- src/views/SelectCoupon.vue
+++ src/views/SelectCoupon.vue
@@ -73,18 +73,14 @@ export default {
           type: 'cancel',
           content: '已取消优惠券'
         })
-        window.setTimeout(() => {
-          this.$router.back()
-        }, 500)
+        this.$router.push({ name: 'OrderSuccess' })
       } else {
         this.$store.dispatch('SELECT_COUPON', coupon)
         vToast({
           type: 'success',
           content: '已选中优惠券'
         })
-        window.setTimeout(() => {
-          this.$router.back()
-        }, 500)
+        this.$router.push({ name: 'OrderSuccess' })
       }
     }
   },
