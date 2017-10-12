5506bc1e97b544e06bb83cd42f2959353d95702b
mizne <w20054319@126.com>
2017-10-12 21:39:23 +0800

    homeImag

--- src/views/SingleTenantHome.vue
+++ src/views/SingleTenantHome.vue
@@ -63,7 +63,7 @@ export default {
     ...mapGetters(['hasClosed', 'homeImage', 'tenantName', 'needDeliveryFee']),
     homeStyle() {
       return {
-        'background': `url(${this.homeImage}) no-repeat`,
+        'background': `url(${this.homeImage}) no-repeat !important`,
       }
     },
   },
