167711ffcf6cd1e6bba2d2088318c5aaa03e9694
mizne <w20054319@126.com>
2017-10-12 15:55:19 +0800

    hongbaojie first

--- src/views/hongbaojie.vue
+++ src/views/hongbaojie.vue
@@ -6,31 +6,17 @@ import { objFrom } from '@/util/index'
 import {queryOpenid, displayOpenid} from '@/http/HongBaoJieService'
 import axios from 'axios'
 export default {
-
     created() {
       if (window.location.search) {
         const obj = objFrom(window.location.search)
         const code = obj.code
-        alert("code:" + code);
         //根据code获取微信openid用户头像
         //将openid用户头像post给会展人后台,完成后跳转到红包节界面
         //return code
         queryOpenid(code).then(resp => {
-            alert("返回结果" + JSON.stringify(resp.data))
-           const OpenId = resp.data.result.OpenId
-//              OpenId:
-//              Headimgurl:
-//              nickname:
-//           return displayOpenid(OpenId).then(resp => {
-//              if(resp.data.resCode === 0){
-//                 window.location.href = `http://dm.huizhanren.com/home/actlist/0d05035c-599f-437c-820e-a6eb009b4e3d?openid=${OpenId}`
-//              } else {
-//                  return ''
-//              }
-//          })
-        }
-        )
-      }
+          window.location.href = `http://dm.huizhanren.com/home/actlist/0d05035c-599f-437c-820e-a6eb009b4e3d?openid=${resp.data.result.openid}`
+     })
     }
+   }
 }
 </script>
